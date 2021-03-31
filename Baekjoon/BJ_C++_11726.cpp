1e2029316e0a0ae1c2cd86332ceb678d7c8d823b
Astrid-DM <65839810+Astrid-DM@users.noreply.github.com>
2021-03-31 15:59:31 +0900

    BAEKJOON(백준) 11726 : Dynamic Programming (C++)
    The explanation : This problem is easy to understand. So I didn't make a note for it

---
+++ Baekjoon/BJ_C++_11726.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n = 0, arr[1001];
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    arr[0] = 0; 
+    arr[1] = 1;
+    arr[2] = 2;
+    for(int i=3; i<=n; i++){
+        arr[i] = (arr[i-2]+arr[i-1])%10007;
+    }
+    cout << arr[n] << "\n";
+    return 0;
+}
