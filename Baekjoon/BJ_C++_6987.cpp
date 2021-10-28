a9b9693a09e96107469da5903a081e0a18b3d2f7
Astrid-DM <65839810+Astrid-DM@users.noreply.github.com>
2021-10-28 14:55:44 +0900

    BAEKJOON(백준) 6987 : Back Tracking (C++)
    The explanation : https://astrid-dm.tistory.com/510

---
+++ Baekjoon/BJ_C++_6987.cpp
@@ -0,0 +1,67 @@
+// The problem link : https://www.acmicpc.net/problem/6987
+// The result : 2020KB, 320ms
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int arr[6][3], goal[6][3], ans[4];
+vector<pair<int, int>> v;
+bool res = true;
+
+void backTracking(int depth) {
+    if (depth == v.size()) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i][j] != goal[i][j]) return;
+            }
+        }
+    }
+    if (depth == v.size()) {
+        res = false;
+        return;
+    }
+    int a = v[depth].first, b = v[depth].second;
+    // a 승
+    arr[a][0]++;
+    arr[b][2]++;
+    backTracking(depth + 1);
+    arr[a][0]--;
+    arr[b][2]--;
+    // 무승부
+    arr[a][1]++;
+    arr[b][1]++;
+    backTracking(depth + 1);
+    arr[a][1]--;
+    arr[b][1]--;
+    // b 승
+    arr[a][2]++;
+    arr[b][0]++;
+    backTracking(depth + 1);
+    arr[a][2]--;
+    arr[b][0]--;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int cnt = 0;
+    for (int i = 0; i < 6; i++) {
+        for (int j = i + 1; j < 6; j++) v.push_back({ i,j });
+    }
+    while (true) {
+        res = true;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = 0;
+                cin >> goal[i][j];
+            }
+        }
+        backTracking(0); // res = true = 실패, res = false = 성공
+        if (!res) ans[cnt] = 1;
+        else ans[cnt] = 0;
+        cnt++;
+        if (cnt == 4) break;
+    }
+    for (int i = 0; i < 4; i++) cout << ans[i] << " ";
+    return 0;
+}
